e1446b8000da2f7af084024c9dd3b4df61106c9c
sirlexis <alex.agyapong@hotmail.com>
2018-04-18 10:47:52 +0100

    Added player class

---
+++ C#/Trivia/Trivia/Player.cs
@@ -0,0 +1,18 @@
+﻿namespace Trivia
+{
+    public class Player
+    {
+        public Player(string name)
+        {
+            Name = name;
+            Purse = 0;
+            Place = 0;
+            IsInPenaltyBox = false;
+        }
+
+        public string Name { get; }
+        public int Purse { get; }
+        public int Place { get; }
+        public bool IsInPenaltyBox { get; }
+    }
+}
